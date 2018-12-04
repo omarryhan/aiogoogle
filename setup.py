690a4640ed9e16530fb97d01ed65cc6b845813dd
Omar Ryhan <omarryhan@gmail.com>
2018-12-04 06:44:25 +0200

    Bumbed version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
 
     name='aiogoogle',
-    version="0.0.1a1",
+    version="0.0.1a2",
     author='Omar Ryhan',
     author_email='omarryhan@gmail.com',
     license='GNU',
