925a9ea63f757df772e0563ac800699f06a9b2cf
Omar Ryhan <omarryhan@gmail.com>
2019-02-10 19:16:49 +0200

    Bumped version again to fix a problem with Pypi

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
 
     name='aiogoogle',
-    version="0.1.2",
+    version="0.1.3",
     author='Omar Ryhan',
     author_email='omarryhan@gmail.com',
     license='GNU',
