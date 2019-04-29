10d8cc9ff80361208d19aa0db5f12aafb1c126e6
Omar Ryhan <omarryhan@gmail.com>
2019-04-30 01:32:24 +0200

    Bumped version. Updated aiogoogle on Pypi

--- docs/conf.py
+++ docs/conf.py
@@ -29,9 +29,9 @@ copyright = '2018, Omar Ryhan'
 author = 'Omar Ryhan'
 
 # The short X.Y version
-version = '0.1.9'
+version = '0.1.10'
 # The full version, including alpha/beta/rc tags
-release = '0.1.9'
+release = '0.1.10'
 
 
 # -- General configuration ---------------------------------------------------
