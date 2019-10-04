7a75ff592336ff60a48d3920d400531d1cffb748
Omar Ryhan <omarryhan@gmail.com>
2019-10-04 22:19:09 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aiogoogle",
-    version="0.1.15",
+    version="0.1.16",
     author="Omar Ryhan",
     author_email="omarryhan@gmail.com",
     license="GNU",
