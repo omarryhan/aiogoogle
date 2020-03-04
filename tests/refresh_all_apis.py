b2a0330c879e7f338d68fdfe17981a0825f5cf86
Omar Ryhan <omarryhan@gmail.com>
2020-03-04 06:31:54 +0200

    Fixed typo

--- tests/refresh_all_apis.py
+++ tests/refresh_all_apis.py
@@ -30,7 +30,7 @@ def _pop_buggy_apis(all_apis: list):
     unstable_apis = [
         {'name': 'toolresults', 'version': 'v1'}
     ]
-    def check_if_api_exists(api):
+    def _is_api_unstable(api):
         for unstable_api in unstable_apis:
             if api[0] == unstable_api[0] and api[1] == unstable_api[1]:
                 return true
