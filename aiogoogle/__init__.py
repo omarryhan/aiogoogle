2cb4b7171daad47ab5a5cc16c98b7301e86d7ca6
jupiterbjy <jupiterbjy@gmail.com>
2022-01-21 19:34:27 +0900

    Explict import of submodule's __all__ & removed 2 unnecessary imports

--- aiogoogle/__init__.py
+++ aiogoogle/__init__.py
@@ -12,18 +12,17 @@ from .__version__ import (  # noqa: F401  imported but unused
     __license__,
     __copyright__,
 )
-from .client import Aiogoogle  # noqa: F401  imported but unused
-from .resource import GoogleAPI  # noqa: F401  imported but unused
+from .client import Aiogoogle, __all__ as _client_all  # noqa: F401  imported but unused
+from .resource import GoogleAPI, __all__ as _resource_all  # noqa: F401  imported but unused
 from .excs import (  # noqa: F401  imported but unused
     AiogoogleError,
     AuthError,
     HTTPError,
     ValidationError,
+    __all__ as _exception_all
 )
-from . import utils  # noqa: F401  imported but unused
-from . import validate  # noqa: F401  imported but unused
 
 
-__all__.extend(client.__all__)
-__all__.extend(resource.__all__)
-__all__.extend(excs.__all__)
+__all__.extend(_client_all)
+__all__.extend(_resource_all)
+__all__.extend(_exception_all)
